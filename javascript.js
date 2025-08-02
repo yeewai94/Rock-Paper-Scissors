4a434754a883991e616289fbbd99c5ef537ad2d3
Anson <43693662+yeewai94@users.noreply.github.com>
2025-08-02 09:01:36 +0800

    Added new div in HTML to store new element created in javascript Rewrite the playGame function to change the display result from console to DOM Added if else statement to stop the game once it reaches 5 wins

--- javascript.js
+++ javascript.js
@@ -38,27 +38,53 @@ function playRound(humanChoice, computerChoice){
 }
 
 function playGame(choice){
+  if (humanScore < 5 && computerScore < 5){
   let humanSelection = getHumanChoice(choice.target.id);
   let computerSelection = getComputerChoice();
+  
+  const result = document.querySelector(".result");
 
-  console.log("You Chose : " + humanSelection);
-  console.log("Computer Chose : " + computerSelection);
-  console.log(playRound(humanSelection, computerSelection));
+  const gameRound = document.createElement("div");
+  const playerChoice = document.createElement("p");
+  const computerChoice = document.createElement("p");
+  const gameResult = document.createElement("p");
+  const finalResult = document.createElement("p");
+
+  playerChoice.innerText = `You Chose : ${humanSelection}`;
+  computerChoice.innerText = `Computer Chose : ${computerSelection}`;
+  gameResult.innerText = `${playRound(humanSelection, computerSelection)}`;
+  finalResult.innerText = `Your Wins : ${humanScore} , Computer Wins : ${computerScore}`;
+
+  result.appendChild(gameRound); 
+  gameRound.appendChild(playerChoice);
+  gameRound.appendChild(computerChoice);
+  gameRound.appendChild(gameResult);
+  gameRound.appendChild(finalResult);
+  
+  }
+  else {
+    alert("GAME OVER!")
+  }
+
 };
+
+
 
 // const paper = document.querySelector("#paper").addEventListener("click", playGame);
 // const rock = document.querySelector("#rock").addEventListener("click", playGame);
 // const scissor = document.querySelector("#scissor").addEventListener("click", playGame);
 
-// const button = document.querySelectorAll("button").forEach(selection => {
-//   selection.addEventListener("click", playGame);
+const button = document.querySelectorAll("button").forEach(selection => {
+  selection.addEventListener("click", playGame);
+});
+
+// document.querySelector("div").addEventListener("click", selection => {
+//   if (selection.target.id === "paper" || selection.target.id === "rock" || selection.target.id === "scissor"){
+//     playGame(selection);
+//   };
 // });
 
-document.querySelector("div").addEventListener("click", selection => {
-  if (selection.target.id === "paper" || selection.target.id === "rock" || selection.target.id === "scissor"){
-    playGame(selection);
-  };
-});
+
 
 
   
